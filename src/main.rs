1f6e9260a07fbee6c0819bbfb587d91eeee8e50d
Christian Carter <cdcarter@gmail.com>
2023-03-05 20:29:42 -0800

    we have an init and a page allocator

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,29 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
-use core::arch::global_asm;
 
+use core::arch::global_asm;
+use core::arch::asm;
 
 global_asm!(include_str!("asm/boot.S"));
 global_asm!(include_str!("asm/trap.S"));
+global_asm!(include_str!("asm/mem.S"));
 
-
-use core::arch::asm;
-
+extern "C" {
+	static TEXT_START: usize;
+	static TEXT_END: usize;
+	static DATA_START: usize;
+	static DATA_END: usize;
+	static RODATA_START: usize;
+	static RODATA_END: usize;
+	static BSS_START: usize;
+	static BSS_END: usize;
+	static KERNEL_STACK_START: usize;
+	static KERNEL_STACK_END: usize;
+	static HEAP_START: usize;
+	static HEAP_SIZE: usize;
+	static mut KERNEL_TABLE: usize;
+}
 // ///////////////////////////////////
 // / RUST MACROS
 // ///////////////////////////////////
@@ -22,7 +36,8 @@ use core::arch::asm;
 macro_rules! print
 {
 	($($args:tt)+) => ({
-
+		use core::fmt::Write;
+		let _ = write!(crate::uart::Uart::new(0x1000_0000), $($args)+);
 	});
 }
 #[macro_export]
@@ -47,7 +62,7 @@ extern "C" fn eh_personality() {}
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
 	print!("Aborting: ");
-	if let Some(_p) = info.location() {
+	if let Some(p) = info.location() {
 		println!(
 		         "line {}, file {}: {}",
 		         p.line(),
@@ -79,14 +94,49 @@ fn abort() -> ! {
 // ///////////////////////////////////
 #[no_mangle]
 extern "C"
-fn kmain() {
+fn kinit() {
 	// Main should initialize all sub-systems and get
 	// ready to start scheduling. The last thing this
 	// should do is start the timer.
+
+	let mut board_uart = uart::Uart::new(0x1000_0000);
+	board_uart.init();
+
+	println!("RISC-V Virt Memory Map");
+	unsafe {
+	println!("TEXT:   0x{:x} -> 0x{:x}", TEXT_START, TEXT_END);
+	println!("RODATA: 0x{:x} -> 0x{:x}", RODATA_START, RODATA_END);
+	println!("DATA:   0x{:x} -> 0x{:x}", DATA_START, DATA_END);
+	println!("BSS:    0x{:x} -> 0x{:x}", BSS_START, BSS_END);
+	println!("STACK:  0x{:x} -> 0x{:x}", KERNEL_STACK_START, KERNEL_STACK_END);
+	println!("HEAP:   0x{:x} -> 0x{:x}", HEAP_START, HEAP_START+HEAP_SIZE);
+	}
+
+	page::init();
+
+	page::alloc(1);
+	page::alloc(2);
+	page::alloc(3);
+	let freeable = page::alloc(1);
+	page::alloc(4);
+	page::dalloc(freeable);
+	page::alloc(4);
+	page::print_page_allocations();
+
+	unsafe {asm!("li t6, 0xdeadbeef")};
+	println!("<kinit>: Complete");
+}
+
+#[no_mangle]
+extern "C"
+fn kinit_hart() {
+	// this is the entry point for the non-primary harts, i guess.
+	unsafe {asm!("wfi")};
 }
 
 // ///////////////////////////////////
 // / RUST MODULES
 // ///////////////////////////////////
 
-
+pub mod uart;
+pub mod page;
---
+++ src/page.rs
@@ -0,0 +1,203 @@
+use core::mem::size_of;
+use core::num;
+use core::ptr::null_mut;
+
+extern "C" {
+    // just reading these symbols is unsafe btw!
+    static HEAP_START: usize;
+    static HEAP_SIZE: usize;
+}
+
+static mut ALLOC_START: usize = 0;
+const PAGE_ORDER: usize = 12;
+pub const PAGE_SIZE: usize = 1 << 12;
+
+pub const fn align_val(val: usize, order: usize) -> usize {
+    let o = (1usize << order) - 1;
+    (val + o) & !o
+}
+
+#[repr(u8)]
+#[derive(Debug)]
+pub enum PageBits {
+    Empty = 0,
+    Taken = 1 << 0,
+    Last = 1 << 1,
+}
+
+impl PageBits {
+    pub fn val(self) -> u8 {
+        self as u8
+    }
+}
+
+#[derive(Debug)]
+pub struct Page {
+    flags: u8,
+}
+
+impl Page {
+    pub fn is_last(&self) -> bool {
+        if self.flags & PageBits::Last.val() != 0 {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_taken(&self) -> bool {
+        if self.flags & PageBits::Taken.val() != 0 {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_free(&self) -> bool {
+        !self.is_taken()
+    }
+
+    pub fn clear(&mut self) {
+        self.flags = PageBits::Empty.val();
+    }
+
+    pub fn set_flag(&mut self, flag: PageBits) {
+        self.flags |= flag.val();
+    }
+
+    pub fn clear_flag(&mut self, flag: PageBits) {
+        self.flags &= !(flag.val());
+    }
+}
+
+pub fn init() {
+    unsafe {
+        let num_pages = HEAP_SIZE / PAGE_SIZE;
+        let ptr = HEAP_START as *mut Page;
+        println!("Clearing {} pages - starting at: {:p}", num_pages, ptr);
+
+        // clear all pages all bits, just in case
+        for i in 0..num_pages {
+            let x = &mut (*ptr.add(i));
+            x.clear();
+        }
+
+        ALLOC_START = align_val(HEAP_START + num_pages * size_of::<Page>(), PAGE_ORDER);
+    }
+}
+
+pub fn alloc(pages: usize) -> *mut u8 {
+    assert!(pages > 0);
+
+    unsafe {
+        let num_pages = HEAP_SIZE / PAGE_SIZE;
+        let ptr = HEAP_START as *mut Page;
+        let mut found = false;
+
+        for i in 0..num_pages - pages {
+            if (*ptr.add(i)).is_free() {
+                found = true;
+                for _ in i..i + pages {
+                    if (!(*ptr.add(i)).is_free()) {
+                        found = false;
+                        break;
+                    }
+                }
+            }
+
+            if found {
+                for k in i..i + pages - 1 {
+                    (*ptr.add(k)).set_flag(PageBits::Taken);
+                }
+
+                (*ptr.add(i + pages - 1)).set_flag(PageBits::Taken);
+                (*ptr.add(i + pages - 1)).set_flag(PageBits::Last);
+
+                return (ALLOC_START + PAGE_SIZE * i) as *mut u8;
+            }
+        }
+    }
+
+    null_mut()
+}
+
+pub fn zalloc(pages: usize) -> *mut u8 {
+    let ret = alloc(pages);
+    if !ret.is_null() {
+        //TODO why isnt this an optional? for rust alloc hook?
+        let size = (PAGE_SIZE * pages) / 8;
+        let big_ptr = ret as *mut u64; // alarm alarm is this broken on 32 bit? does it matter?
+        for i in 0..size {
+            // we're forcing a store doubleword here to make this fast
+            // probably should try asm instead?
+            unsafe {
+                (*big_ptr.add(i)) = 0;
+            }
+        }
+    }
+    ret
+}
+
+pub fn dalloc(ptr: *mut u8) {
+    assert!(!ptr.is_null());
+    unsafe {
+        let addr = HEAP_START + (ptr as usize - ALLOC_START) / PAGE_SIZE;
+        assert!(addr >= HEAP_START && addr < HEAP_START + HEAP_SIZE);
+        let mut p = addr as *mut Page;
+        while (*p).is_taken() && !(*p).is_last() {
+            (*p).clear();
+            p = p.add(1);
+        }
+        assert!((*p).is_last() == true, "Possible double-free detected");
+        (*p).clear();
+    }
+}
+
+pub fn print_page_allocations() {
+    unsafe {
+        let num_pages = HEAP_SIZE / PAGE_SIZE;
+        let mut beg = HEAP_START as *const Page;
+        let end = beg.add(num_pages);
+        let alloc_beg = ALLOC_START;
+        let alloc_end = ALLOC_START + num_pages * PAGE_SIZE;
+        println!();
+        println!(
+            "PAGE ALLOCATION TABLE\nMETA: {:p} -> {:p}\nPHYS: \
+		          0x{:x} -> 0x{:x}",
+            beg, end, alloc_beg, alloc_end
+        );
+        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        let mut num = 0;
+        while beg < end {
+            if (*beg).is_taken() {
+                let start = beg as usize;
+                let memaddr = ALLOC_START + (start - HEAP_START) * PAGE_SIZE;
+                print!("0x{:x} => ", memaddr);
+                loop {
+                    num += 1;
+                    if (*beg).is_last() {
+                        let end = beg as usize;
+                        let memaddr = ALLOC_START + (end - HEAP_START) * PAGE_SIZE + PAGE_SIZE - 1;
+                        print!("0x{:x}: {:>3} page(s)", memaddr, (end - start + 1));
+                        println!(".");
+                        break;
+                    }
+                    beg = beg.add(1);
+                }
+            }
+            beg = beg.add(1);
+        }
+        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        println!(
+            "Allocated: {:>5} pages ({:>9} bytes).",
+            num,
+            num * PAGE_SIZE
+        );
+        println!(
+            "Free     : {:>5} pages ({:>9} bytes).",
+            num_pages - num,
+            (num_pages - num) * PAGE_SIZE
+        );
+        println!();
+    }
+}
