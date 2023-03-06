7e5a8a058c3cf4570b1f49924b91d18fbcae2a0b
Christian Carter <cdcarter@gmail.com>
2023-03-05 20:42:38 -0800

    bugs in page allocator

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,13 @@
 #![no_main]
 #![feature(panic_info_message)]
 
-use core::arch::global_asm;
 use core::arch::asm;
+use core::arch::global_asm;
 
 global_asm!(include_str!("asm/boot.S"));
 global_asm!(include_str!("asm/trap.S"));
 global_asm!(include_str!("asm/mem.S"));
 
-extern "C" {
-	static TEXT_START: usize;
-	static TEXT_END: usize;
-	static DATA_START: usize;
-	static DATA_END: usize;
-	static RODATA_START: usize;
-	static RODATA_END: usize;
-	static BSS_START: usize;
-	static BSS_END: usize;
-	static KERNEL_STACK_START: usize;
-	static KERNEL_STACK_END: usize;
-	static HEAP_START: usize;
-	static HEAP_SIZE: usize;
-	static mut KERNEL_TABLE: usize;
-}
 // ///////////////////////////////////
 // / RUST MACROS
 // ///////////////////////////////////
@@ -61,82 +46,106 @@ macro_rules! println
 extern "C" fn eh_personality() {}
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-	print!("Aborting: ");
-	if let Some(p) = info.location() {
-		println!(
-		         "line {}, file {}: {}",
-		         p.line(),
-		         p.file(),
-		         info.message().unwrap()
-		);
-	}
-	else {
-		println!("no information available.");
-	}
-	abort();
+    print!("Aborting: ");
+    if let Some(p) = info.location() {
+        println!(
+            "line {}, file {}: {}",
+            p.line(),
+            p.file(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("no information available.");
+    }
+    abort();
 }
 #[no_mangle]
-extern "C"
-fn abort() -> ! {
-	loop {
-		unsafe {
-			asm!("wfi");
-		}
-	}
+extern "C" fn abort() -> ! {
+    loop {
+        unsafe {
+            asm!("wfi");
+        }
+    }
 }
 
 // ///////////////////////////////////
 // / CONSTANTS
 // ///////////////////////////////////
+extern "C" {
+    static TEXT_START: usize;
+    static TEXT_END: usize;
+    static DATA_START: usize;
+    static DATA_END: usize;
+    static RODATA_START: usize;
+    static RODATA_END: usize;
+    static BSS_START: usize;
+    static BSS_END: usize;
+    static KERNEL_STACK_START: usize;
+    static KERNEL_STACK_END: usize;
+    static HEAP_START: usize;
+    static HEAP_SIZE: usize;
+}
 
 // ///////////////////////////////////
 // / ENTRY POINT
 // ///////////////////////////////////
 #[no_mangle]
-extern "C"
-fn kinit() {
-	// Main should initialize all sub-systems and get
-	// ready to start scheduling. The last thing this
-	// should do is start the timer.
+extern "C" fn kinit() {
+    // Main should initialize all sub-systems and get
+    // ready to start scheduling. The last thing this
+    // should do is start the timer.
 
-	let mut board_uart = uart::Uart::new(0x1000_0000);
-	board_uart.init();
+    let mut board_uart = uart::Uart::new(0x1000_0000);
+    board_uart.init();
 
-	println!("RISC-V Virt Memory Map");
-	unsafe {
-	println!("TEXT:   0x{:x} -> 0x{:x}", TEXT_START, TEXT_END);
-	println!("RODATA: 0x{:x} -> 0x{:x}", RODATA_START, RODATA_END);
-	println!("DATA:   0x{:x} -> 0x{:x}", DATA_START, DATA_END);
-	println!("BSS:    0x{:x} -> 0x{:x}", BSS_START, BSS_END);
-	println!("STACK:  0x{:x} -> 0x{:x}", KERNEL_STACK_START, KERNEL_STACK_END);
-	println!("HEAP:   0x{:x} -> 0x{:x}", HEAP_START, HEAP_START+HEAP_SIZE);
-	}
+    println!("RISC-V Virt Memory Map");
+    unsafe {
+        println!("TEXT:   0x{:x} -> 0x{:x}", TEXT_START, TEXT_END);
+        println!("RODATA: 0x{:x} -> 0x{:x}", RODATA_START, RODATA_END);
+        println!("DATA:   0x{:x} -> 0x{:x}", DATA_START, DATA_END);
+        println!("BSS:    0x{:x} -> 0x{:x}", BSS_START, BSS_END);
+        println!(
+            "STACK:  0x{:x} -> 0x{:x}",
+            KERNEL_STACK_START, KERNEL_STACK_END
+        );
+        println!(
+            "HEAP:   0x{:x} -> 0x{:x}",
+            HEAP_START,
+            HEAP_START + HEAP_SIZE
+        );
+    }
 
-	page::init();
+    page::init();
 
-	page::alloc(1);
-	page::alloc(2);
-	page::alloc(3);
-	let freeable = page::alloc(1);
-	page::alloc(4);
-	page::dalloc(freeable);
-	page::alloc(4);
+    println!("allocating 1 pages: {:p}",page::zalloc(1));
+    println!("allocating 2 pages: {:p}",page::zalloc(2));
+    println!("allocating 3 pages: {:p}",page::zalloc(3));
+    let freeable = page::zalloc(2);
+	println!("allocating 2 pages: {:p} ** this will be freed",freeable);
+    println!("allocating 4 pages: {:p}",page::zalloc(4));
+    page::print_page_allocations();
+
+    println!("deallocating 2 pages: {:p}", freeable);
+    page::dalloc(freeable);
 	page::print_page_allocations();
+    println!("allocating 4 pages: {:p}",page::zalloc(4));
+	println!("allocating 1 pages: {:p}",page::zalloc(1));
+    println!("allocating 1 pages: {:p}",page::zalloc(1));
+    page::print_page_allocations();
 
-	unsafe {asm!("li t6, 0xdeadbeef")};
-	println!("<kinit>: Complete");
+    unsafe { asm!("li t6, 0xdeadbeef") };
+    println!("<kinit>: Complete");
 }
 
 #[no_mangle]
-extern "C"
-fn kinit_hart() {
-	// this is the entry point for the non-primary harts, i guess.
-	unsafe {asm!("wfi")};
+extern "C" fn kinit_hart() {
+    // this is the entry point for the non-primary harts, i guess.
+    unsafe { asm!("wfi") };
 }
 
 // ///////////////////////////////////
 // / RUST MODULES
 // ///////////////////////////////////
 
-pub mod uart;
 pub mod page;
+pub mod uart;
--- src/page.rs
+++ src/page.rs
@@ -97,8 +97,8 @@ pub fn alloc(pages: usize) -> *mut u8 {
         for i in 0..num_pages - pages {
             if (*ptr.add(i)).is_free() {
                 found = true;
-                for _ in i..i + pages {
-                    if (!(*ptr.add(i)).is_free()) {
+                for j in i..i + pages {
+                    if (!(*ptr.add(j)).is_free()) {
                         found = false;
                         break;
                     }
@@ -154,50 +154,60 @@ pub fn dalloc(ptr: *mut u8) {
 }
 
 pub fn print_page_allocations() {
-    unsafe {
-        let num_pages = HEAP_SIZE / PAGE_SIZE;
-        let mut beg = HEAP_START as *const Page;
-        let end = beg.add(num_pages);
-        let alloc_beg = ALLOC_START;
-        let alloc_end = ALLOC_START + num_pages * PAGE_SIZE;
-        println!();
-        println!(
-            "PAGE ALLOCATION TABLE\nMETA: {:p} -> {:p}\nPHYS: \
+	unsafe {
+		let num_pages = HEAP_SIZE / PAGE_SIZE;
+		let mut beg = HEAP_START as *const Page;
+		let end = beg.add(num_pages);
+		let alloc_beg = ALLOC_START;
+		let alloc_end = ALLOC_START + num_pages * PAGE_SIZE;
+		println!();
+		println!(
+		         "PAGE ALLOCATION TABLE\nMETA: {:p} -> {:p}\nPHYS: \
 		          0x{:x} -> 0x{:x}",
-            beg, end, alloc_beg, alloc_end
-        );
-        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        let mut num = 0;
-        while beg < end {
-            if (*beg).is_taken() {
-                let start = beg as usize;
-                let memaddr = ALLOC_START + (start - HEAP_START) * PAGE_SIZE;
-                print!("0x{:x} => ", memaddr);
-                loop {
-                    num += 1;
-                    if (*beg).is_last() {
-                        let end = beg as usize;
-                        let memaddr = ALLOC_START + (end - HEAP_START) * PAGE_SIZE + PAGE_SIZE - 1;
-                        print!("0x{:x}: {:>3} page(s)", memaddr, (end - start + 1));
-                        println!(".");
-                        break;
-                    }
-                    beg = beg.add(1);
-                }
-            }
-            beg = beg.add(1);
-        }
-        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        println!(
-            "Allocated: {:>5} pages ({:>9} bytes).",
-            num,
-            num * PAGE_SIZE
-        );
-        println!(
-            "Free     : {:>5} pages ({:>9} bytes).",
-            num_pages - num,
-            (num_pages - num) * PAGE_SIZE
-        );
-        println!();
-    }
+		         beg, end, alloc_beg, alloc_end
+		);
+		println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		let mut num = 0;
+		while beg < end {
+			if (*beg).is_taken() {
+				let start = beg as usize;
+				let memaddr = ALLOC_START
+				              + (start - HEAP_START)
+				                * PAGE_SIZE;
+				print!("0x{:x} => ", memaddr);
+				loop {
+					num += 1;
+					if (*beg).is_last() {
+						let end = beg as usize;
+						let memaddr = ALLOC_START
+						              + (end
+						                 - HEAP_START)
+						                * PAGE_SIZE
+						              + PAGE_SIZE - 1;
+						print!(
+						       "0x{:x}: {:>3} page(s)",
+						       memaddr,
+						       (end - start + 1)
+						);
+						println!(".");
+						break;
+					}
+					beg = beg.add(1);
+				}
+			}
+			beg = beg.add(1);
+		}
+		println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		println!(
+		         "Allocated: {:>5} pages ({:>9} bytes).",
+		         num,
+		         num * PAGE_SIZE
+		);
+		println!(
+		         "Free     : {:>5} pages ({:>9} bytes).",
+		         num_pages - num,
+		         (num_pages - num) * PAGE_SIZE
+		);
+		println!();
+	}
 }
